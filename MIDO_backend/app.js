07cb6fc0d1294d332399931723a17efaa7ba1436
manzcube <your@email.com>
2023-02-06 18:44:51 +0100

    dploy config

--- MIDO_backend/app.js
+++ MIDO_backend/app.js
@@ -27,7 +27,11 @@ connectDB();
 // App initialization
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://mido.onrender.com",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 // We want all transmited data in json before being read
 app.use(json());
 
